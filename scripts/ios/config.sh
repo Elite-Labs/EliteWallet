e654ced2679d6f8bec8fb1e7165a53086e09a17e
Elite-Labs <info@elitewallet.sc>
2024-03-30 18:13:19 +0000

    bugfix

--- scripts/ios/config.sh
+++ scripts/ios/config.sh
@@ -11,28 +11,7 @@ export ELITEWALLET_DATA_DIR=${EXTERNAL_IOS_SOURCE_DIR}/elite_wallet_data
 export LOCAL_GIT_DEPS=${ELITEWALLET_DATA_DIR}/deps
 export BUILD_TYPE="simulator"
 export LOCAL_GIT_DEPS_SUBDIR=${LOCAL_GIT_DEPS}/${BUILD_TYPE}
-echo $IOS_SCRIPTS_DIR
-LAST_DEPS_CHANGE_GITHASH=$(find "$IOS_SCRIPTS_DIR" -type f -exec shasum -a 256 {} + | shasum -a 256 | cut -c1-6)
-
-SUBMODULE_HASHES=""
-
-# Loop through each submodule
-git submodule foreach |
-while read -r line; do
-    submodule_path=$(echo "$line" | awk '{ print $2 }')
-    submodule_path=${submodule_path//\'/}
-    submodule_hash=$(git -C "$submodule_path" rev-parse HEAD)
-    
-    if [ -n "$submodule_hash" ]; then
-        SUBMODULE_HASHES="$SUBMODULE_HASHES$submodule_hash\n"
-    else
-        echo "Error: Unable to get hash for submodule: $submodule_path"
-        exit 1
-    fi
-done
-
-# Calculate the combined hash of all submodule hashes
-export LAST_DEPS_CHANGE_GITHASH=$(echo -e "$LAST_DEPS_CHANGE_GITHASH$SUBMODULE_HASHES" | shasum -a 256 | cut -c1-6)
+export LAST_DEPS_CHANGE_GITHASH=$(find "$IOS_SCRIPTS_DIR" -type f -exec shasum -a 256 {} + | shasum -a 256 | cut -c1-6)
 echo "Combined hash: $LAST_DEPS_CHANGE_GITHASH"
 export CURRENT_DEPS=${LOCAL_GIT_DEPS_SUBDIR}/${LAST_DEPS_CHANGE_GITHASH}
 
